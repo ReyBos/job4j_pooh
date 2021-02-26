6cffeeb0e2e301076c17c79195b5bfb1a82370c4
reybos <andreybosiy@yandex.ru>
2021-02-26 17:36:22 +0300

    1. Техническое задание - проект "Pooh JMS" [#456187]

--- src/main/java/ru/job4j/Broker.java
+++ src/main/java/ru/job4j/Broker.java
@@ -4,23 +4,26 @@ import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Broker {
     private static final Logger LOG = LoggerFactory.getLogger(Broker.class.getName());
-    private final ConcurrentHashMap<String, Queue<String>> queues;
-    private final ConcurrentHashMap<String, Queue<String>> topics;
-    private final ConcurrentHashMap<String, Function<JsonObject, JsonObject>> dispatch;
+    private final ConcurrentHashMap<String, LinkedBlockingQueue<String>> queues =
+            new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, LinkedBlockingQueue<String>> topics =
+            new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Function<JsonObject, JsonObject>> dispatch =
+            new ConcurrentHashMap<>();
 
     public Broker() {
-        queues = new ConcurrentHashMap<>();
-        topics = new ConcurrentHashMap<>();
-        dispatch = new ConcurrentHashMap<>();
         queues.put("weather", new LinkedBlockingQueue<>());
-        topics.put("weather", new LinkedBlockingQueue<>());
+        topics.put("weather.#", new LinkedBlockingQueue<>());
+        topics.put("*.Moscow", new LinkedBlockingQueue<>());
+        topics.put("weather.Moscow", new LinkedBlockingQueue<>());
         dispatch.put("queue", this.toQueue());
         dispatch.put("topic", this.toTopic());
     }
@@ -30,13 +33,18 @@ public class Broker {
             JsonObject resp = new JsonObject();
             String action = json.get("action").getAsString();
             String key = json.get("key").getAsString();
-            Queue<String> queue = queues.get(key);
+            LinkedBlockingQueue<String> queue = queues.get(key);
             if (action.equals("POST")) {
                 String text = json.get("text").getAsString();
                 queue.offer(text);
                 resp.addProperty("text", "добавлено в очередь");
             } else if (action.equals("GET")) {
-                String text = queue.poll();
+                String text = null;
+                try {
+                    text = queue.take();
+                } catch (InterruptedException e) {
+                    LOG.error("Ошибка ", e);
+                }
                 resp.addProperty("text", text);
             }
             return resp;
@@ -48,13 +56,26 @@ public class Broker {
             JsonObject resp = new JsonObject();
             String action = json.get("action").getAsString();
             String key = json.get("key").getAsString();
-            Queue<String> topic = topics.get(key);
             if (action.equals("POST")) {
                 String text = json.get("text").getAsString();
-                topic.offer(text);
+                for (String topicKey : topics.keySet()) {
+                    String topicKeyPattern = topicKey.replaceAll("#", "\\.+");
+                    topicKeyPattern = topicKeyPattern.replaceAll("\\*", "\\.+");
+                    Pattern pattern = Pattern.compile(topicKeyPattern);
+                    Matcher matcher = pattern.matcher(key);
+                    if (matcher.find()) {
+                        topics.get(topicKey).offer(text);
+                    }
+                }
                 resp.addProperty("text", "добавлено в топики");
             } else if (action.equals("GET")) {
-                String text = topic.poll();
+                LinkedBlockingQueue<String> topic = topics.get(key);
+                String text = null;
+                try {
+                    text = topic.take();
+                } catch (InterruptedException e) {
+                    LOG.error("Ошибка ", e);
+                }
                 resp.addProperty("text", text);
             }
             return resp;
@@ -63,7 +84,6 @@ public class Broker {
 
     public JsonObject process(JsonObject req) {
         String mode = req.get("mode").getAsString();
-        JsonObject resp = dispatch.get(mode).apply(req);
-        return resp;
+        return dispatch.get(mode).apply(req);
     }
 }
